06be688f813d19825d63141b553d84dfc068e087
ashwal015 <ashwalse015@gmail.com>
2026-07-27 11:59:49 +0200

    Point frontend API baseURL to EC2 public IP

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
-});
+    baseURL: "http://54.93.243.198:8000",
+});   /* baseURL: "http://localhost:8000", */
 
 export function setAuthToken(token) {
     if (token) {
